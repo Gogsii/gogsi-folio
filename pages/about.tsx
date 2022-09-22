39484d73e30bf21830b5e200693191e4e7c9ab85
gogsi <dragutinovic.g@gmail.com>
2022-09-22 14:37:48 -0700

    new design

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,7 @@ import avatarBW from 'public/avatar-bw.jpg';
 
 export default function About() {
   return (
-    <Container title="About – Lee Robinson">
+    <Container title="About – Goran Dragutinovic">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           About Me
@@ -16,67 +16,71 @@ export default function About() {
           <h2>Links</h2>
           <ul>
             <li>
-              Twitter: <a href="https://twitter.com/leeerob">@leeerob</a>
+              Twitter: <a href="https://twitter.com/gogsii">@gogsii</a>
             </li>
             <li>
-              GitHub: <a href="https://github.com/leerob">@leerob</a>
+              GitHub: <a href="https://github.com/gogsii">@gogsi</a>
             </li>
             <li>
               Website:{' '}
-              <Link href="https://leerob.io">
-                <a>https://leerob.io</a>
+              <Link href="https://dragutinov.com">
+                <a>https://dragutinov.com</a>
               </Link>
             </li>
             <li>
               LinkedIn:{' '}
-              <a href="https://www.linkedin.com/in/leeerob/">
-                https://www.linkedin.com/in/leeerob
+              <a href="https://www.linkedin.com/in/dragutinovic/">
+                https://www.linkedin.com/in/dragutinovic
               </a>
             </li>
           </ul>
           <h2>Bio</h2>
           <h3>Job Title</h3>
-          <p>Lee Robinson, VP of Developer Experience at Vercel</p>
+          <p>
+            Goran Dragutinovic, Co-Founder at Golden Ape Media | SEO @ All Star
+            Directories
+          </p>
           <h3>Long, 3rd Person</h3>
           <p>
-            Lee Robinson is the VP of Developer Experience at{' '}
-            <a href="http://vercel.com/">Vercel</a>, where he helps developers
-            build a faster web and leads the Next.js community. He leads the
-            community for the React framework Next.js and is an open-source
-            contributor. An educator, writer, and speaker, Lee has created
-            courses on React, Next.js, and web development.
+            Goran Dragutinovic is the Co-Founder at Golden Ape Media, where he
+            helps companies build and manage Google friendly websites. He is
+            also the SEO Specialist for All Star Directories. An entrepreneur by
+            nature, Goran has created websites that generated close to one
+            million monthly visits. A graduate of Finance, he fell in love with
+            the blockchain and Web3 and is currently self-learning the Solidity
+            language.
           </p>
           <h3>Long, 1st Person</h3>
           <p>
-            Hey, I'm Lee. I'm the VP of Developer Experience at{' '}
-            <a href="https://vercel.com/">Vercel</a>, where my team helps
-            developers build a faster web. I'm a Next.js contributor and help
-            lead our open-source communities. I'm passionate about frontend
-            development and have created courses on React, Next.js, and web
-            development. I'm an advisor and investor in early stage startups.
+            Hey, I'm Goran. I'm the Co-Founder at Golden Ape Media, where we
+            help companies build and manage Google friendly websites. I have
+            built websites that generated close to one million monthly visitors.
+            I'm passionate about programming and am continously learning to
+            program blockchain smart contracts with Solidity, as well as
+            practicing front-end with React, and Next.js.
           </p>
           <h3>Short, 3rd Person</h3>
           <p>
-            Lee Robinson is the VP of Developer Experience at{' '}
-            <a href="http://vercel.com/">Vercel</a>, where he helps developers
-            build a faster web.
+            Goran Dragutinovic is the Co-Founder at Golden Ape Media, where he
+            helps companies build and manage Google friendly websites. Also, SEO
+            specialist at All Star Directories.
           </p>
           <h3>Short, 1st Person</h3>
           <p>
-            Hey, I'm Lee. I'm the VP of Developer Experience at{' '}
-            <a href="https://vercel.com/">Vercel</a>, where my team helps
-            developers build a faster web.
+            Hey, I'm Goran. I'm SEO Specialist at All Star Directories, where I
+            help build better, more user and Google friendly sites.
           </p>
           <h3>Education</h3>
           <p>
-            Lee Robinson graduated from Iowa State University with a B.S. in
-            Computer Engineering.
+            Goran Dragutinovic graduated from Cleveland State University with a
+            B.A. in Finance. Also completed the Full-Stack Web Development
+            bootcamp from Nucamp Coding.
           </p>
           <h2>Headshots</h2>
           <div className="flex space-x-8">
             <a href="/avatar.jpg">
               <Image
-                alt="Lee Robinson headshot"
+                alt="Goran Dragutinovic headshot"
                 width={400}
                 quality={100}
                 src={avatar}
@@ -85,7 +89,7 @@ export default function About() {
             </a>
             <a href="/avatar-bw.jpg">
               <Image
-                alt="Lee Robinson headshot"
+                alt="Goran Dragutinovic headshot"
                 width={400}
                 quality={100}
                 src={avatarBW}
