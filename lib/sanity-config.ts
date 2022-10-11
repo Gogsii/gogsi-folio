c2eaa2b32bad61941435169ebd8d616bab1d69dc
gogsi <dragutinovic.g@gmail.com>
2022-10-11 14:25:24 -0700

    updated sanity tokens

--- lib/sanity-config.ts
+++ lib/sanity-config.ts
@@ -1,8 +1,6 @@
-import 'dotenv/config'
-
 export const sanityConfig = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_API_DATASET || 'production',
+  projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
   useCdn: process.env.NODE_ENV !== 'production',
   apiVersion: '2021-03-25'
 }
